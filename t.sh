f06c11142fb05c98d27e4aa6bebb4bef4a65d9ba
Axel Tetzlaff <axel.tetzlaff@gmx.de>
2011-07-22 20:56:48 +0200

    proper filenames etc

--- t.sh
+++ t.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm vec3.h vec3.c
-cabal-dev build && ./dist/build/daemon/daemon vec3.class && echo "--------" && g++ -c -g -o vec3.o  vec3.c && echo 'Success!!'
+cabal-dev build && ./dist/build/cheap/cheap vec3.class && echo "--------" && g++ -c -g -o vec3.o  vec3.c && echo 'Success!!'
